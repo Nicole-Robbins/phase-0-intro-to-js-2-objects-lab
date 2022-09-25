adfa5974438257d32a976ba55bc7140f4c04fb62
Nicole Robbins <nicole.robbins@att.net>
2022-09-25 15:45:07 -0500

    commit tree b5c94baa720fd94dc64d73ededcd13e56eb07b77

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+    name: "Stormy",
+    street: "Bigleaf"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const updateEmployee = {...employee};
+    delete updateEmployee[key];
+    return updateEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
